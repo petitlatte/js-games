4d3a2bc078e9867626199a90295bac9d80888946
petitlatte <lisasumida@gmail.com>
2020-01-31 15:45:07 +1300

    debugged obstacles

--- flappy bird/js/script.js
+++ flappy bird/js/script.js
@@ -1,12 +1,14 @@
 ////////////////////////////////////EMPTY VARIABLES////////////////////////////////////////////
-var gamePiece;	
+var gamePiece;
+var obstacles;	
 
 
 //////////////////////////////////////FUNCTIONS///////////////////////////////////////////////
 
 	function startGame(){
-				gameCanvas.start();
 				gamePiece = new component(30,30,"teal",10,120);
+				obstacles = new component(10, 200, "red", 300,120);
+				gameCanvas.start();
 	}
 
 
@@ -21,16 +23,19 @@ var gamePiece;
 			this.interval = setInterval(updateGameArea, 20);
 			window.addEventListener("keydown",function(e){
 				gameCanvas.key =e.keyCode;
-			})
+			});
 			window.addEventListener("keyup",function(e){
 				gameCanvas.key = false;
-			})
+			});
 
 		},
 		clear: function(){
 			this.context.clearRect(0,0,this.canvas.width, this.canvas.height);
+		},
+		stop: function(){
+			clearInterval(this.interval);
 		}
-	}
+	};
 
 	//gamepiece component
 	function component(width,height,color,x,y){
@@ -45,24 +50,43 @@ var gamePiece;
 		ctx.fillStyle = color;
 		ctx.fillRect(this.x,this.y,this.width,this.height);
 		}
-		this.newPos = function(){
-			this.x += this.speedX;
-			this.y += this.speedY;
+
+  this.newPos = function() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+  }
+
+		this.crashWith = function(otherobj) {
+			var myLeft = this.x;
+			var myRight = this.x + (this.width);
+			var myTop = this.y;
+			var myBottom = this.y + (this.height);
+			var otherLeft = otherobj.x; //error?
+			var otherRight = otherobj.x + (otherobj.width);
+			var otherTop = otherobj.y;
+			var otherBottom = otherobj.y + (otherobj.height);
+			var crash = true;
+			if ((myBottom < otherTop) || (myTop > otherBottom) ||(myRight < otherLeft) || (myLeft > otherRight)) {
+				crash = false;
 		}
-	}	
+		return crash;
+	};
+}	
 
 	//update game area
 	function updateGameArea(){
+		if (gamePiece.crashWith(obstacles)) {
+			gameCanvas.stop();
+		} else {
 		gameCanvas.clear();
-		gamePiece.speedX = 0;
-		gamePiece.speedY = 0;
-		if (gameCanvas.key && gameCanvas.key == 37){ gamePiece.speedX= -1; }
-		if (gameCanvas.key && gameCanvas.key == 39){ gamePiece.speedX=  1; }
-		if (gameCanvas.key && gameCanvas.key == 38){ gamePiece.speedY= -1; }
-		if (gameCanvas.key && gameCanvas.key == 40){ gamePiece.speedY=  1; }						
+		obstacles.x += -1;		
+		obstacles.update();
+        gamePiece.x += gamePiece.speedX;
+        gamePiece.y += gamePiece.speedY; 					
 		gamePiece.newPos();
-		gamePiece.update();
+		gamePiece.update();	
 	}
+}
 
 	function moveUp(){
 		gamePiece.speedY -= 1;
